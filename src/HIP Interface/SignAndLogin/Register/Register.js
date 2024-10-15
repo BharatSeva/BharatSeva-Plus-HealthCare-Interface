05808463351548a076c857d5b2dee8e51f193021
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-15 20:04:35 +0530

    Improving the allignment of Register page

--- src/HIP Interface/SignAndLogin/Register/Register.js
+++ src/HIP Interface/SignAndLogin/Register/Register.js
@@ -60,120 +60,304 @@ export default function Register() {
     }
 
     return (
-        <>
-            <div className="RegisterOuterContainerHealthCare">
-                <div className={`HIP_RegisterContainer DisplayFlexjustifyAlignitem ${Fetched.IsFetched ? "DisplayOpacity" : ""}`}>
-                    <div className="RegisterLable">
-                        <p>HealthCare Registration 🩺</p>
-                        <p><span><strong>Note</strong></span> : After Successfull Registration <br></br> You have to Login for Dashboard.</p>
+			<>
+				<div className="RegisterOuterContainerHealthCare">
+					<div
+						className={`HIP_RegisterContainer DisplayFlexjustifyAlignitem ${
+							Fetched.IsFetched ? "DisplayOpacity" : ""
+						}`}>
+						<div className="RegisterLable">
+							<p>HealthCare Registration 🩺</p>
+							<p>
+								<span>
+									<strong>Note</strong>
+								</span>{" "}
+								: After Successfull Registration <br></br> You have to Login for
+								Dashboard.
+							</p>
 
-                        <ul className="RegisterPagetxt">
-                            <li>Healthcare ID Must be 10 Characters Long.</li>
-                            <li>Keep License Number Same as Healthcare ID for the shake of simplicity.</li>
-                            <li>All the inputs have limits try to avoid them. </li>
-                            <li>If you have any feedback or you Encountered Unusual Error you can simply <a href="mailto:21vaibhav11@gmail.com">Mail</a> Me</li>
-                            <li>If you are Flutter developer or want to build your own project and want to use my API you can <a href="mailto:21vaibhav11@gmail.com">Mail</a> Me regarding this. I'm happy to help 😊</li>
-                        </ul>
+							<ul className="RegisterPagetxt">
+								<li>Healthcare ID Must be 10 Characters Long.</li>
+								<li>
+									Keep License Number Same as Healthcare ID for the shake of
+									simplicity.
+								</li>
+								<li>All the inputs have limits try to avoid them. </li>
+								<li>
+									If you have any feedback or you Encountered Unusual Error you
+									can simply <a href="mailto:21vaibhav11@gmail.com">Mail</a> Me
+								</li>
+								<li>
+									If you are Flutter developer or want to build your own project
+									and want to use my API you can{" "}
+									<a href="mailto:21vaibhav11@gmail.com">Mail</a> Me regarding
+									this. I'm happy to help 😊
+								</li>
+							</ul>
+						</div>
+						<div className="RegisterBox">
+							<p className="WelcomeGreetings">
+								Welcome To Health Care Registration Portal
+							</p>
 
-                    </div>
-                    <div className="RegisterBox">
-                        <p className="WelcomeGreetings">Welcome To Health Care Registration Portal</p>
+							<form onSubmit={RegisterAPIGOESHere}>
+								<label>Health Care Name :</label>
+								<input
+									type="text"
+									placeholder="Enter Health Care Name"
+									name="healthcareName"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
+								<label>
+									Health Care ID :{" "}
+									<span className="HelptextinRegister">
+										(HealthCare ID Should Be Unique)
+									</span>
+								</label>
+								<input
+									type="number"
+									placeholder="Unique ID to Identify You"
+									name="healthcareId"
+									onChange={OnclickChange}
+									required
+								/>
+								{/* <p className="HelptextinRegister">HealthCare ID Shoudl Be Unique</p> */}
+								<br></br>
+								<label>License Number :</label>
+								<input
+									type="number"
+									placeholder="Same As Your HealthCare Number"
+									name="healthcarelicense"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                        <form onSubmit={RegisterAPIGOESHere}>
-                            <label>Health Care Name :</label>
-                            <input type="text" placeholder="Enter Health Care Name" name="healthcareName" onChange={OnclickChange} required />
-                            <br></br>
-                            <label>Health Care ID : <span className="HelptextinRegister">(HealthCare ID Should Be Unique)</span></label>
-                            <input type="number" placeholder="Unique ID to Identify You" name="healthcareId" onChange={OnclickChange} required />
-                            {/* <p className="HelptextinRegister">HealthCare ID Shoudl Be Unique</p> */}
-                            <br></br>
-                            <label>License Number :</label>
-                            <input type="number" placeholder="Same As Your HealthCare Number" name="healthcarelicense" onChange={OnclickChange} required />
-                            <br></br>
+								<label>State :</label>
+								<input
+									type="text"
+									placeholder="Enter State"
+									name="state"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>State :</label>
-                            <input type="text" placeholder="Enter State" name="state" onChange={OnclickChange} required /><br></br>
+								<label>City</label>
+								<input
+									type="text"
+									placeholder="Enter City"
+									name="city"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>City</label>
-                            <input type="text" placeholder="Enter City" name="city" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Country</label>
+								<input
+									type="text"
+									placeholder="Enter Country"
+									name="country"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Country</label>
-                            <input type="text" placeholder="Enter Country" name="country" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Landmark</label>
+								<input
+									type="text"
+									placeholder="Enter Landmark"
+									name="landmark"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Landmark</label>
-                            <input type="text" placeholder="Enter Landmark" name="landmark" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Email :</label>
+								<input
+									type="email"
+									placeholder="Email Must Be Unique"
+									name="email"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Email :</label>
-                            <input type="email" placeholder="Email Must Be Unique" name="email" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Appointment Fee :</label>
+								<input
+									type="number"
+									placeholder="Enter Appointment Fee"
+									name="appointment_fee"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Appointment Fee :</label>
-                            <input type="number" placeholder="Enter Appointment Fee" name="appointment_fee" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Availability :</label>
+								<input
+									type="text"
+									placeholder="Enter Availability"
+									name="availability"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Availability :</label>
-                            <input type="text" placeholder="Enter Availability" name="availability" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Total Facilites :</label>
+								<input
+									type="number"
+									placeholder="Enter Total Facilites"
+									name="total_facilities"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Total Facilites :</label>
-                            <input type="number" placeholder="Enter Total Facilites" name="total_facilities" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Total MBBS Doctors :</label>
+								<input
+									type="number"
+									placeholder="Enter Total MBBS Doctors"
+									name="total_mbbs_doc"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Total MBBS Doctors :</label>
-                            <input type="number" placeholder="Enter Total MBBS Doctors" name="total_mbbs_doc" onChange={OnclickChange} required />
-                            <br></br>
+								<label>Total Workers :</label>
+								<input
+									type="number"
+									placeholder="Enter Total Workers"
+									name="total_worker"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>Total Workers :</label>
-                            <input type="number" placeholder="Enter Total Workers" name="total_worker" onChange={OnclickChange} required />
-                            <br></br>
+								<label>No. Of Beds :</label>
+								<input
+									type="number"
+									placeholder="Enter No. Of Beds"
+									name="no_of_beds"
+									onChange={OnclickChange}
+									required
+								/>
+								<br></br>
 
-                            <label>No. Of Beds :</label>
-                            <input type="number" placeholder="Enter No. Of Beds" name="no_of_beds" onChange={OnclickChange} required />
-                            <br></br>
+								<div
+									className="registerHealthCaretextareaContainer"
+									style={{
+										display: "flex",
+										flexDirection: "row",
+										alignItems: "center",
+										marginLeft: "-16px",
+									}}>
+									<label className="registerHealthCaretextarea">
+										About Your Hospital :
+									</label>
+									<textarea
+										style={{ width: "60%" }}
+										className="registerHealthCaretextarea"
+										type="text"
+										placeholder="Enter About Your Hospital"
+										name="about"
+										onChange={OnclickChange}
+										required
+										rows="5"
+										cols="39"></textarea>
+								</div>
 
-                            <div className="registerHealthCaretextareaContainer">
-                                <label className="registerHealthCaretextarea">About Your Hospital :</label>
-                                <textarea className="registerHealthCaretextarea" type="text" placeholder="Enter About Your Hospital" name="about" onChange={OnclickChange} required rows="5" cols="39"></textarea>
-                            </div>
+								<label>Password :</label>
+								<input
+									type="password"
+									placeholder="Enter Password"
+									id="Registration_Password"
+									required
+								/>
+								<br></br>
+								<label>Password Again :</label>
+								<input
+									type="password"
+									placeholder="Enter Your Password Again"
+									name="password"
+									id="Registration_CheckPassword"
+									onChange={OnclickChange}
+									required
+								/>
+								<p
+									id="RegisterPasswordStatus"
+									className="DiplayNone"></p>
 
-                            <label>Password :</label>
-                            <input type="password" placeholder="Enter Password" id="Registration_Password" required />
-                            <br></br>
-                            <label>Password Again :</label>
-                            <input type="password" placeholder="Enter Your Password Again" name="password" id="Registration_CheckPassword" onChange={OnclickChange} required />
-                            <p id="RegisterPasswordStatus" className="DiplayNone"></p>
+								<div
+									className="Registerbtn DisplayFlexjustifyAlignitem"
+									style={{ width: "90%", margin: "0px auto" }}>
+									<button style={{ width: "100%" }}>Register*</button>
+								</div>
+							</form>
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "row",
+									marginTop: "30px",
+									justifyContent: "center",
+									alignItems: "center",
+									gap: "10px",
+								}}>
+								<div
+									style={{
+										width: "45%",
+										height: "1px",
+										backgroundColor: "whitesmoke",
+									}}></div>
+								<div style={{ fontSize: "1.5rem", fontWeight: "bolder" , color: "white"}}>
+									OR
+								</div>
+								<div
+									style={{
+										width: "45%",
+										height: "1px",
+										backgroundColor: "whitesmoke",
+									}}></div>
+							</div>
+							<p className="LoginbtnRedirect">
+								Already Registered! <Link to="/healthcare/login">Login</Link>
+							</p>
 
-                            <div className="Registerbtn DisplayFlexjustifyAlignitem">
-                                <button>Register*</button>
-                                <p className="TermsandConditions">*By Clicking On Register You Agree To Our Terms and Conditions!</p>
-                            </div>
+							<p
+								style={{ textAlign: "center", marginTop: "20px" }}
+								className="TermsandConditions">
+								*By Clicking On Register You Agree To Our Terms and Conditions!
+							</p>
+						</div>
+					</div>
+				</div>
 
-                        </form>
-                        <p className="LoginbtnRedirect">Already Registered! <Link to="/healthcare/login">Login</Link></p>
-                    </div>
-                </div>
+				{/* This One For PopBox In register */}
+				{Fetched.IsFetched && (
+					<div className="Popoverdropbox displayFlexWithR">
+						<div className="PopOvercontainerBoxregisterpage displayFlexWithR">
+							<h1>{Status}</h1>
+							{Fetched.IsRedirect ? (
+								<NavLink to="/healthcare/login">Login</NavLink>
+							) : (
+								Fetched.IsGood && (
+									<button
+										id="AtregisterPage"
+										onClick={() =>
+											SetIsFetched((p) => ({ IsGood: false, IsFetched: false }))
+										}>
+										Continue
+									</button>
+								)
+							)}
+						</div>
+					</div>
+				)}
 
-
-
-            </div>
-
-            {/* This One For PopBox In register */}
-            {Fetched.IsFetched && (
-                <div className="Popoverdropbox displayFlexWithR">
-                    <div className="PopOvercontainerBoxregisterpage displayFlexWithR">
-                        <h1>{Status}</h1>
-                        {Fetched.IsRedirect ? (<NavLink to='/healthcare/login'>Login</NavLink>) : (Fetched.IsGood && (<button id="AtregisterPage" onClick={() => SetIsFetched((p) => ({ IsGood: false, IsFetched: false }))}>Continue</button>))}
-                    </div>
-                </div>
-            )}
-            
-            {/* This one is for Popup */}
-            {/* <InsecureContent /> */}
-
-        </>
-    )
+				{/* This one is for Popup */}
+				{/* <InsecureContent /> */}
+			</>
+		);
 }
 // (<button id="AtregisterPage" onClick={() => SetIsFetched((p) => ({ IsGood: false, IsFetched: false }))}>Continue</button>)
