a3c21b1a9cce46694cbe83ddbbd91332ba2042c8
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2024-10-19 11:19:16 +0100

    Adding codes to message.js

--- src/Message.js
+++ src/Message.js
@@ -6,7 +6,7 @@ export default function Message({ message }) {
     return (
         <div className="AllMessageBharatSeva MessageDisplayNone container">Message us Here
         <form>
-           <h1>Message us</h1>
+           {/*<h1>Message us</h1>*/}
             <input type="text" id="firstName" placeholder="First Name" required/>
             <input type="text" id="lastName" placeholder="Last Name" required/>
             <input type="email" id="email" placeholder="Email" required/>
