b881a568a9b36670c6c4d3d70aeb491ca895fc45
Aniket Baghel <88363603+Bamof25th@users.noreply.github.com>
2024-10-22 00:19:49 +0530

    Fixed

--- src/HIP Interface/Dashboard/NavBar/NavBar.js
+++ src/HIP Interface/Dashboard/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import { Outlet, NavLink } from "react-router-dom"
+import { Outlet, NavLink } from "react-router-dom" //Link is removed
 import "./NavBar.css"
 
 export default function NavBar({ toggleSideBar }) {
@@ -13,52 +13,51 @@ export default function NavBar({ toggleSideBar }) {
         document.querySelector(".HealthCare_AccountPopOver").classList.toggle("DisplayNone")
     }
 
-    // This Will Get the Hospital Name
+    // This Will Get the Hopital Name
     const HealthCare = JSON.parse(sessionStorage.getItem("BharatSevahealthCare"))
     return (
         <>
             <div className="SideBarContainer">
-                <nav className="HomePageNavBarContainer">
-                    <div className="NavBarContent">
-                        <div className="LeftSideNavBar">
-                            <i className="fa-solid fa-bars" onClick={() => toggleSideBar()}></i>
-                            <p className="CompanyName">Bharat सेवा+</p>
+                <div className="HomePageNavBarContainer DisplayFlexjustifyAlignitem">
+
+                    <div className="LeftSideNavBar DisplayFlexjustifyAlignitem">
+                        <i className="fa-solid fa-bars" onClick={() => toggleSideBar()}></i>
+                        <p className="CompanyName">Bharat सेवा+</p>
+                    </div>
+
+                    <div className="LeftSideNavBar_NameTag DisplayFlexjustifyAlignitem">
+                        <p className="HealthcareNameUser"><i className="fa-solid fa-user-doctor"></i> Health Care InterFace</p>
+                    </div>
+
+                    <div className="LeftSideNavBar_AccountAndNotification DisplayFlexjustifyAlignitem">
+                        <p className="DisplayFlexjustifyAlignitem transition4OneSecond" onClick={ToggleNotificationPopOver}><i className="fa-regular fa-bell"></i> Notification
+                        </p>
+                        <div className="HealthCare_NotificationPopOver DisplayNone">
+                            <ul>
+                                <li>Welcome to Bharat Seva HealthCare Dashboard</li>
+                                <li>This App is still Under-development :)</li>
+                                <li>Star this project!</li>
+                            </ul>
                         </div>
 
-                        <div className="LeftSideNavBar_NameTag">
-                            <p className="HealthcareNameUser"><i className="fa-solid fa-user-doctor"></i> Health Care InterFace</p>
-                        </div>
+                        {/* Account Section Goes here */}
+                        <p className="DisplayFlexjustifyAlignitem transition4OneSecond classACcountBharatseva" onClick={HealthCare_ToggleAccountPopUp}><i className="fa-solid fa-user"></i> Account</p>
 
-                        <div className="LeftSideNavBar_AccountAndNotification">
-                            <button className="NavButton" onClick={ToggleNotificationPopOver}>
-                                <i className="fa-regular fa-bell"></i> Notification
-                            </button>
-                            <div className="HealthCare_NotificationPopOver DisplayNone">
-                                <ul>
-                                    <li>Welcome to Bharat Seva HealthCare Dashboard</li>
-                                    <li>This App is still Under-development :)</li>
-                                    <li>Star this project!</li>
-                                </ul>
-                            </div>
+                        <div className="HealthCare_AccountPopOver DisplayNone">
+                            <ul>
+                                <li style={{ color: "lime" }}>Signed In As</li>
+                                <li className="HealthcareNameLableAccount">{HealthCare.name}</li>
+                                <hr></hr>
+                                <li>  <a target="__blank" href="https://github.com/CaptainTron/BharatSeva-User-Interface/discussions">Go to Discussion</a></li>
+                                <li> <a target="__blank" href="https://github.com/CaptainTron/BharatSeva-HealthCare-Interface">Star this Project ⭐</a></li>
+                                <li>API Integrations</li>
+                                <li onClick={() => sessionStorage.clear()}><NavLink to='/healthcare/login' className="LogoutHealthcare" >Log Out</NavLink></li>
 
-                            <button className="NavButton" onClick={HealthCare_ToggleAccountPopUp}>
-                                <i className="fa-solid fa-user"></i> Account
-                            </button>
-                            <div className="HealthCare_AccountPopOver DisplayNone">
-                                <ul>
-                                    <li style={{ color: "lime" }}>Signed In As</li>
-                                    <li className="HealthcareNameLableAccount">{HealthCare.name}</li>
-                                    <hr></hr>
-                                    <li>  <a target="__blank" href="https://github.com/CaptainTron/BharatSeva-User-Interface/discussions">Go to Discussion</a></li>
-                                    <li> <a target="__blank" href="https://github.com/CaptainTron/BharatSeva-HealthCare-Interface">Star this Project ⭐</a></li>
-                                    <li>API Integrations</li>
-                                    <li onClick={() => sessionStorage.clear()}><NavLink to='/healthcare/login' className="LogoutHealthcare" >Log Out</NavLink></li>
-
-                                </ul>
-                            </div>
+                            </ul>
                         </div>
                     </div>
-                </nav>
+
+                </div>
             </div>
             <Outlet />
         </>
